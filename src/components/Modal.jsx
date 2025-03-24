d6b1fa7a14aab8f1d4e70b4014c16df720c774ad
arpit jain <jain.arpit@northeastern.edu>
2025-03-23 22:59:41 -0500

    sign in with added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 const Modal = ({ isOpen, onClose }) => {
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${
-        isOpen ? '' : 'hidden'
+        isOpen ? "" : "hidden"
       }`}
     >
       <div className="modal-container">
         <div className="bg-indigo-700 text-center p-5 h-96 w-[500px] rounded shadow-md">
           {/* Modal content, including login form */}
-          <h2 className="text-xl font-semibold mb-4 mt-6 mb-5 uppercase">Please Login Here!</h2>
-          <form className='px-4'>
-            {/* email */}
-             <div className="mb-5">
+          {!isAuthenticated && (
+            <h2 className="text-xl font-semibold mb-4 mt-6 mb-5 uppercase">
+              Please Login Here!
+            </h2>
+          )}
+          {!isAuthenticated && (
+            <form className="px-4">
+              {/* email */}
+              <div className="mb-5">
                 <input
                   type="email"
                   name="email"
@@ -21,9 +35,9 @@ const Modal = ({ isOpen, onClose }) => {
                   placeholder="example@domain.com"
                   className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
-            </div>
-            {/* password */}
-            <div className="mb-5">
+              </div>
+              {/* password */}
+              <div className="mb-5">
                 <input
                   type="password"
                   name="emapasswordil"
@@ -31,26 +45,40 @@ const Modal = ({ isOpen, onClose }) => {
                   placeholder="type your password"
                   className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
-            </div>
-
-            {/* submit btn */}
-            <button
-                  className="hover:shadow-form rounded-md bg-[#6A64F1] hover:bg-orange-600 py-3 px-8 text-base font-semibold text-white outline-none"
-                >
-                  Login
-                </button>
-          </form>
+              </div>
+              {/* submit btn */}
+              <button className="hover:shadow-form rounded-md bg-[#6A64F1] hover:bg-orange-600 py-3 px-8 text-base font-semibold text-white outline-none">
+                Login
+              </button>
+              &nbsp; &nbsp; 
+              <button
+                className="hover:shadow-form rounded-md bg-[#6A64F1] hover:bg-orange-600 py-3 px-8 text-base font-semibold text-white outline-none"
+                onClick={() => loginWithRedirect()}
+              >
+                Sign in with
+              </button>
+            </form>
+          )}
           {/* Add your login form here */}
+          {isAuthenticated && (
+            <button
+              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded inline-flex items-center mt-8"
+              onClick={handleLogout}
+            >
+              Sign out
+            </button>
+          )}
+          &nbsp; &nbsp; &nbsp; &nbsp;
           <button
-            onClick={onClose}
             className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded inline-flex items-center mt-8"
+            onClick={onClose}
           >
             Close
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,64 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { Home } from './pages/Home.jsx';
-import Services from './pages/Services.jsx';
-import About from './pages/About.jsx';
-import { Blogs } from './pages/Blogs.jsx';
-import Contact from './pages/Contact.jsx';
-import SingleBlog from './pages/SingleBlog.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home.jsx";
+import Services from "./pages/Services.jsx";
+import About from "./pages/About.jsx";
+import { Blogs } from "./pages/Blogs.jsx";
+import Contact from "./pages/Contact.jsx";
+import SingleBlog from "./pages/SingleBlog.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
       {
         path: "/",
-        element: <Home/>
+        element: <Home />,
       },
       {
         path: "/services",
-        element: <Services/>
+        element: <Services />,
       },
       {
         path: "/about",
-        element: <About/>
+        element: <About />,
       },
       {
         path: "/blogs",
-        element: <Blogs/>
+        element: <Blogs />,
       },
       {
         path: "/contact",
-        element: <Contact/>
+        element: <Contact />,
       },
       {
         path: "/blogs/:id",
-        element: <SingleBlog/>,
-        loader:  ({ params }) => fetch(`http://localhost:5000/blogs/${params.id}`)
-      }
-    ]
+        element: <SingleBlog />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/blogs/${params.id}`),
+      },
+    ],
   },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>
+    {/* <RouterProvider router={router} /> */}
+  </React.StrictMode>
+);
